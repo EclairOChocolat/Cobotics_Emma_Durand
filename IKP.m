f44d8e6dc9a259ea3da1ff6f1084619a328a7ec4
EclairOChocolat <131017579+EclairOChocolat@users.noreply.github.com>
2025-01-12 14:33:24 +0100

    Add files via upload

--- IKP.m
+++ IKP.m
@@ -1,6 +1,5 @@
  function [theta1, theta2, rho] = IKP(xi, yi, zi)
-
-    l = 20;
+     l = 20;
     d = 50;
     h = 45 ;
     %Calcul de theta1 :
@@ -10,12 +9,14 @@
     theta1 = psi - a;
     
     %Calcul de theta2:
-    theta2 = - atan2((zi - h), (r - l));
+    theta2 = -atan2((zi - h), (r*cos(a)));
+
 
     %Calcul de rho :
-    r2 = sqrt(xi^2 + (yi-l)^2);
-    rho = sqrt((r2)^2 + (zi - h)^2) - d;
-
+    r2 = sqrt(xi^2 + yi^2+zi^2);
+    %rho = 
+    rho = sqrt((r2 - l)^2 + (zi - h)^2) - d;
 
 end
+
 
